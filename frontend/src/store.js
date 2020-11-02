d4ae409af6f49bf0df9425a124469c6f9e29219b
NIKESH GHIMIRE <nikeshghimire77@gmail.com>
2020-11-02 11:14:03 -0600

    Tested and finalized the list member details on admin screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,6 +14,7 @@ import {
   userUpdateProfileReducer,
   userListReducer,
   userDeleteReducer,
+  userUpdateReducer,
 } from './reducers/userReducers';
 import {
   orderCreateReducer,
@@ -31,6 +32,7 @@ const reducer = combineReducers({
   userDetails: userDetailReducer,
   userUpdateProfile: userUpdateProfileReducer,
   userDelete: userDeleteReducer,
+  userUpdate: userUpdateReducer,
   userList: userListReducer,
   orderCreate: orderCreateReducer,
   orderDetail: orderDetailReducer,
