7fc820d75cacb3b5f1aea95dd71ff5d6b86a4976
NIKESH GHIMIRE <nikeshghimire77@gmail.com>
2020-11-02 11:22:35 -0600

    Fixed the bug on edit user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -149,6 +149,8 @@ const updateUser = asyncHandler(async (req, res) => {
   if (user) {
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
+    user.isAdmin =
+      req.body.isAdmin === undefined ? user.isAdmin : req.body.isAdmin;
 
     const updatedUser = await user.save();
 
